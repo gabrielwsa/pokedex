a4f73431d9bfcd7b69a505a5e49222b8d8621d93
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-05-06 14:23:39 -0300

    configuraciones iniciales

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
+import { join } from 'path'; // pacote node
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PokemonModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // podemos adicionar esse prefixo para que seja o padrão de todas as rotas
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
