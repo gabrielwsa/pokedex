301ef35e6e33867dabc6d97aea3e4e3e837a0776
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-05-06 14:35:54 -0300

    configuraciones de la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { join } from 'path'; // pacote node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
     PokemonModule,
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
   ],
   controllers: [],
   providers: [],
